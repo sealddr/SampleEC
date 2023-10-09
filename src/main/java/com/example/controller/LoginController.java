d04e0b017a5148b0dc027943be26e358e475eaaf
Masakazu Kadoshita <mkadoshita@gmail.com>
2023-10-09 12:38:03 +0900

    Fixed redirect processing when login is successful

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -1,19 +1,27 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class LoginController {
+
+	@Autowired
+	private HttpSession session;
 	
 	@GetMapping("/login")
-	public String getBuyLogin() {
+	public String getLogin() {
 		return "login";
 	}
 	
 	@PostMapping("/login")
-	public String postBuyLogin() {
+	public String postLogin(Model model) {
+		model.addAttribute("cart", session.getAttribute("cart"));
 		return "redirect:/cart/view";
 	}
 }
