1866b49b161a25dd7ac78ee48c35ee7d7bd3d97a
Masakazu Kadoshita <mkadoshita@gmail.com>
2023-10-09 09:45:38 +0900

    Rename LoginController

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -3,19 +3,17 @@ package com.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/buy")
-public class BuyLoginController {
+public class LoginController {
 	
 	@GetMapping("/login")
 	public String getBuyLogin() {
-		return "buy/login";
+		return "login";
 	}
 	
 	@PostMapping("/login")
 	public String postBuyLogin() {
-		return "redirect:/buy/payselect";
+		return "redirect:/cart/view";
 	}
 }
