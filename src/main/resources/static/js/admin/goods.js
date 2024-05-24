d16416fca2901715bc017a5dd25e21d947b1d486
Masakazu Kadoshita <mkadoshita@gmail.com>
2024-05-24 10:58:34 +0900

    bodyのスクロールを有効無効を動的に切り替える

--- src/main/resources/static/js/admin/goods.js
+++ src/main/resources/static/js/admin/goods.js
@@ -5,7 +5,8 @@ registerButton.addEventListener('click', function() {
     // elementのcssにdisplay: none;が設定されている場合display: block;に変更する
     element.style.display = 'block';
 
-
+    // bodyのスクロールを無効にする
+    document.body.style.overflow = 'hidden';
 });
 
 var registerSubmitButton = document.getElementById('btn-register-goods-submit');
@@ -14,6 +15,9 @@ registerSubmitButton.addEventListener('click', function() {
     var element = document.getElementById('register-goods-area');
     element.style.display = 'none';
 
+    // bodyのスクロールを有効にする
+    document.body.style.overflow = 'auto';
+
 });
 
 var registerCancelButton = document.getElementById('btn-register-goods-cancel');
@@ -21,5 +25,8 @@ var registerCancelButton = document.getElementById('btn-register-goods-cancel');
 registerCancelButton.addEventListener('click', function() {
     var element = document.getElementById('register-goods-area');
     element.style.display = 'none';
+
+    // bodyのスクロールを有効にする
+    document.body.style.overflow = 'auto';
 
 });
