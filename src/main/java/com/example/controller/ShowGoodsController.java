8a426463b6e2af27f1a6e9a5792700a714f44d6b
masakazu.kadoshita <mkadoshita@gmail.com>
2024-06-12 10:54:37 +0900

    Refactor ShowGoodsController to remove unused import statement

--- src/main/java/com/example/controller/ShowGoodsController.java
+++ src/main/java/com/example/controller/ShowGoodsController.java
@@ -1,6 +1,5 @@
 package com.example.controller;
 
-import org.springframework.security.access.method.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
