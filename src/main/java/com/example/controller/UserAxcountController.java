4449e0a79a3865f17b6a1479414bcc662b7e9274
Masakazu Kadoshita <mkadoshita@gmail.com>
2024-06-28 13:02:20 +0900

    メールアドレス変更フォームを作成

--- src/main/java/com/example/controller/UserAxcountController.java
+++ src/main/java/com/example/controller/UserAxcountController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.user.model.MUser;
+import com.example.form.UpdateMailAddressForm;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -18,7 +20,9 @@ public class UserAxcountController {
     private HttpSession session;
     
     @GetMapping("/account")
-    public String getAccount(Model model) {
+    public String getAccount(
+        @ModelAttribute UpdateMailAddressForm updateMailAddressForm,
+        Model model) {
         MUser user = (MUser) session.getAttribute("loginUser");
         model.addAttribute("user", user);
         return "user/account";
---
+++ src/main/java/com/example/form/UpdateMailAddressForm.java
@@ -0,0 +1,9 @@
+package com.example.form;
+
+import lombok.Data;
+
+@Data
+public class UpdateMailAddressForm {
+    private String mailAddress;
+    
+}
